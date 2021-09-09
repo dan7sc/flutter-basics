b82a573f471cb114fc5dad1f10d1724e8f756313
dan7sc <dan7sc@users.noreply.github.com>
2021-09-09 13:42:57 -0300

    Add TabPage

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return ScaffoldExample();
+    return TabPage();
     // return Scaffold(
     //   appBar: AppBar(
     //     title: Text(widget.title ?? ''),
